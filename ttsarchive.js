55f355867cbb5b9c8b22eb5d696aa9263d15ebe4
Stefan Kendall <stefankendall@gmail.com>
2016-07-04 17:20:00 -0400

    Download images in game files

--- ttsarchive.js
+++ ttsarchive.js
@@ -6,9 +6,7 @@ program.version('0.0.0');
 program
     .command('archive [path]')
     .description('run setup commands for all envs')
-    .option("-b, --base_url [base_url]", "Base url to replace on all assets")
     .action(function (path, options) {
-        console.log(path);
         archive.archive(path);
     });
 
