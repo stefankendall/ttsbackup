94df645e37cba9ee5c3b2ccc407a06f69858de8a
Stefan Kendall <stefankendall@gmail.com>
2016-07-04 22:33:33 -0400

    Prompt for all necessary information.

--- ttsbackup.js
+++ ttsbackup.js
@@ -1,11 +1,10 @@
 var program = require('commander');
-var archive = require('./lib/commands/archive');
+var download = require('./lib/commands/download');
 var rewriter = require('./lib/commands/rewrite');
 var mod_lister = require('./lib/commands/mod_lister');
 var interactive_backup = require('./lib/commands/interactive_backup');
 
 program.version('0.0.0');
-
 
 program
     .command('backup')
@@ -13,10 +12,10 @@ program
     .action(interactive_backup.run);
 
 program
-    .command('archive <path>')
+    .command('download <path>')
     .option('-c, --clean', 'removes all downloaded game files from archive/Game/Images, Models, and Workshop before starting the archive process')
     .description('download all images, models, and the mod file needed to load in tts')
-    .action(archive.archive);
+    .action(download.download);
 
 program.command('rewrite <path>')
     .description('rewrite urls in the provided mod json file')
