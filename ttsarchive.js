e830a64669809386d9ac435a4c203fad348e77a1
Stefan Kendall <stefankendall@gmail.com>
2016-07-04 21:25:37 -0400

    mod lister lists mods.

--- ttsarchive.js
+++ ttsarchive.js
@@ -1,33 +1,28 @@
 var program = require('commander');
-var archive = require('./lib/archive');
-var rewriter = require('./lib/rewrite');
-var _ = require('lodash');
+var archive = require('./lib/commands/archive');
+var rewriter = require('./lib/commands/rewrite');
+var mod_lister = require('./lib/commands/mod_lister');
 
 program.version('0.0.0');
 
 program
-    .command('archive [path]')
-    .option('-c, --clean', 'removes all downloaded game files before downloading files')
+    .command('archive <path>')
+    .option('-c, --clean', 'removes all downloaded game files from archive/Game/Images, Models, and Workshop before starting the archive process')
     .description('download all images, models, and the mod file needed to load in tts')
-    .action(function (path, options) {
-        archive.archive(path, options);
-    });
+    .action(archive.archive);
 
-program.command('rewrite [path]')
-    .description('rewrite urls in the provided json file')
+program.command('rewrite <path>')
+    .description('rewrite urls in the provided mod json file')
     .option('-b, --base_url <path>', 'the base url for all assets, e.g. https://dl.dropboxusercontent.com/u/5142994/games/MyGame')
-    .action(function (path, options) {
-        if (_.isUndefined(path)) {
-            console.error("Must supply a .json file to overwrite");
-            process.exit("1");
-        }
+    .action(rewriter.rewrite);
 
-        if (_.isUndefined(options.base_url)) {
-            console.error('must supply a base url to replace in the json file');
-            process.exit("1");
-        }
-
-        rewriter.rewrite(path, options);
-    });
+program.command('list [options]')
+    .description('list installed mods')
+    .option('-i, --input_file <path>', "The WorkshopFileInfos.json path. On OSX, this defaults to /Users/<user>/My Games/Tabletop Simulator/Mods/Workshop/WorkshopFileInfos.json")
+    .action(mod_lister.list);
 
 program.parse(process.argv);
+
+if (process.argv.length <= 2) {
+    program.help();
+}
