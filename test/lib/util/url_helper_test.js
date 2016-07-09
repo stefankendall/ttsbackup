dbc43c0747e4664e24ea187a5b508c4aed51e40e
Stefan Kendall <stefankendall@gmail.com>
2016-07-09 16:18:38 -0400

    Leave ?dl=1 on files when downloading, which without breaks some mod backups

--- test/lib/util/url_helper_test.js
+++ test/lib/util/url_helper_test.js
@@ -2,10 +2,10 @@ var assert = require('chai').assert;
 var UrlHelper = require('../../../lib/util/url_helper');
 
 describe('UrlHelper', function () {
-    describe('#removeSpecialSuffixesOnUrl()', function () {
+    describe('#removeSuffixesOnUrl()', function () {
         it('should remove symbols from filenames that cause issues with reading or writing', function () {
-            assert.equal(UrlHelper.removeSpecialSuffixesOnUrl("test{Unique}"), "test");
-            assert.equal(UrlHelper.removeSpecialSuffixesOnUrl("test?dl=1"), "test");
+            assert.equal(UrlHelper.removeSuffixesOnUrl("test{Unique}", UrlHelper.ttsExtensions()), "test");
+            assert.equal(UrlHelper.removeSuffixesOnUrl("test?dl=1", UrlHelper.dropboxExtensions()), "test");
         });
     });
 
