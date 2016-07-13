b4c4cfdec4ca4553d224307c0d0d3fd20521233c
Stefan Kendall <stefankendall@gmail.com>
2016-07-13 15:53:56 -0400

    Remove double spaces in filenames.

--- test/lib/util/file_name_helper_test.js
+++ test/lib/util/file_name_helper_test.js
@@ -6,5 +6,9 @@ describe('FileNameHelper', function() {
         it('should remove symbols from filenames that cause issues with reading or writing', function() {
             assert.equal(FileNameHelper.safe('a:?/\\$,[]{}()!#b.json'), 'ab.json');
         });
+
+        it('should remove double spaces', function(){
+            assert.equal(FileNameHelper.safe('a  b.json'), 'a b.json');
+        });
     });
 });
