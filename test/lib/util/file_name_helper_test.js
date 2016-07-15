5a6b1343f4fc261bf85d78b2cc800ec4657a76c0
Stefan Kendall <stefankendall@gmail.com>
2016-07-14 21:32:56 -0400

    Remove asterisks from filenames for windows

--- test/lib/util/file_name_helper_test.js
+++ test/lib/util/file_name_helper_test.js
@@ -1,14 +1,18 @@
 var assert = require('chai').assert;
 var FileNameHelper = require('../../../lib/util/file_name_helper');
 
-describe('FileNameHelper', function() {
-    describe('#safe()', function() {
-        it('should remove symbols from filenames that cause issues with reading or writing', function() {
+describe('FileNameHelper', function () {
+    describe('#safe()', function () {
+        it('should remove symbols from filenames that cause issues with reading or writing', function () {
             assert.equal(FileNameHelper.safe('a:?/\\$,[]{}()!#b.json'), 'ab.json');
         });
 
-        it('should remove double spaces', function(){
+        it('should remove double spaces', function () {
             assert.equal(FileNameHelper.safe('a  b.json'), 'a b.json');
+        });
+
+        it('removes asterisks for windows', function () {
+            assert.equal(FileNameHelper.safe('a * b.json'), 'a b.json');
         });
     });
 });
